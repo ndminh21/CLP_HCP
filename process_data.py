f5454a383f0457d3e184ad94e0b730b0d9eacf67
minh.nd <minh.nd@teko.vn>
2019-06-24 17:17:25 +0700

    "List all solution"

--- process_data.py
+++ process_data.py
@@ -1,5 +1,5 @@
-f = open("alb1000.hcp", "r")
+f = open("i324.hcp", "r")
 for x in f:
     input = x.strip().split()
-    print("next_node("+input[0]+","+input[1]+").")
-    print("next_node("+input[1]+","+input[0]+").")
+    print("next_node(" + input[0] + "," + input[1] + ").")
+    print("next_node(" + input[1] + "," + input[0] + ").")
--- test.pl
+++ test.pl
@@ -6,6 +6,7 @@
 
 %Representation of the graph.
 %next_node(+Current Node, -A Next Node that is connected to Current Node).
+
 next_node(1,2).
 next_node(2,1).
 next_node(1,3).
@@ -16,8 +17,51 @@ next_node(2,3).
 next_node(3,2).
 next_node(2,4).
 next_node(4,2).
-next_node(3,4).
-next_node(4,3).
+next_node(3,5).
+next_node(5,3).
+next_node(4,6).
+next_node(6,4).
+next_node(5,6).
+next_node(6,5).
+next_node(5,7).
+next_node(7,5).
+next_node(6,8).
+next_node(8,6).
+next_node(7,8).
+next_node(8,7).
+next_node(7,9).
+next_node(9,7).
+next_node(8,10).
+next_node(10,8).
+next_node(9,10).
+next_node(10,9).
+next_node(9,11).
+next_node(11,9).
+next_node(10,12).
+next_node(12,10).
+next_node(11,12).
+next_node(12,11).
+next_node(11,13).
+next_node(13,11).
+next_node(12,14).
+next_node(14,12).
+next_node(13,14).
+next_node(14,13).
+next_node(13,15).
+next_node(15,13).
+next_node(14,16).
+next_node(16,14).
+next_node(15,17).
+next_node(17,15).
+next_node(15,18).
+next_node(18,15).
+next_node(16,17).
+next_node(17,16).
+next_node(16,18).
+next_node(18,16).
+next_node(17,18).
+next_node(18,17).
+
 
 
 %Go! Prints out a solution. Will print out more if you fail the first one.
@@ -92,8 +136,22 @@ res(M,N) :-
     N1 is N-1,
     res(M,N1).
 
-res(M,0) :-!.
+res(_,0) :-!.
 
+
+minh(M,N,[HeadResult|TailResult]) :-
+    N > 0, !,
+    %write('----------'),nl,
+    %write(N), nl,
+    ((ham_it_up(M, N, [N], [HeadResult|TailResult]),
+    write_solution(HeadResult, [HeadResult|TailResult]), nl); true),
+    N1 is N-1,
+    minh(M,N1, [HeadResult|TailResult]).
+
+minh(_,0,_) :-!.
+
+
+hcp(N):-findall(X, minh(N,N, X), XList).
 
 ham_it_up(1, CurrentNode, [Next|RestCurrentPath], [Next|RestCurrentPath]) :-
 
@@ -126,5 +184,3 @@ write_solution(Start, [HeadPath|TailPath]):-
 	write(', '),
 
 	write_solution(Start, TailPath).
-
-%http://www.cs.uky.edu/ai/benchmark-suite/hamiltonian-cycle.html
